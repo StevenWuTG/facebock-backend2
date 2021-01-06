7708b466048920cd37ede0c24cb9b6468cff7bf8
StevenWuTG <stevenwu91@icloud.com>
2021-01-06 11:23:53 -0500

    new backend smell

--- app/controllers/api/v1/walls_controller.rb
+++ app/controllers/api/v1/walls_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::WallsController < ApplicationController
+    def index
+        allWalls = Wall.all
+        render json: allWalls, except: [:created_at, :updated_at]
+    end
+
+    def create
+        wall = Wall.create!(wall_params)
+        if wall.valid?
+            render json: wall,  except: [:created_at, :updated_at]
+        else
+            render json: {errors: wall.errors} 
+        end 
+    end
+
+    def show
+        wall = Wall.find(params[:id])
+        render json: wall, except: [:created_at, :updated_at]
+    end
+
+    def update
+        wall = Wall.find(params[:id])
+        if (wall.update!(post_params))
+            render json: wall
+        else
+            render json: {errors: wall.errors} 
+        end 
+    end 
+
+    def destroy
+        allWall = Wall.all
+        wall = Wall.find(params[:id])
+        wall.destroy
+        render json: allWall
+    end
+
+    private
+
+    def wall_params
+        params.permit(:user)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
+    validates :username, uniqueness: true
+
     has_one :wall
     has_many :posts, through: :wall
 end
